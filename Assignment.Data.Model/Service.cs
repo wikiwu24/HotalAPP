67bf6870562c583c19ca2d1ff08217e80a7bd36b
wikiwu24 <70457942+wikiwu24@users.noreply.github.com>
2021-10-08 15:25:58 -0400

    upload data models

---
+++ Assignment.Data.Model/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentApp.Data.Model
+{
+    public class Service
+    {
+        public int Id { get; set; }
+        public string SDESC { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime ServiceDate { get; set; }
+        public Room Room { get; set; }
+    }
+}
